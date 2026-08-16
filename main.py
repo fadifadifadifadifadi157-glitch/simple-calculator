d20b046e82f39bf7876d4f50f89fd29ba0c21bf5
fadifadifadifadifadi157-glitch <fadifadifadifadifadi157@gmail.com>
2026-08-16 23:42:37 +0500

    Add input validation to calculator

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
+def Calculator(Num1, Num2,Choice):
+    if Choice =="+":
+        return Num1 + Num2
+    elif Choice== "-":
+        return Num1- Num2
+    elif Choice =="*":
+        return Num1 * Num2
+    elif Choice =="/":
+        if Num2 == 0:
+            return "Cannot divide by zero!"
+        return Num1/ Num2
+    else:
+        return "Invalid operation!"
+
 while True:
-    Num1= int(input("Enter the first number: "))
-    Num2= int(input("Enter the second number: "))
+    try:
+        Num1 =int(input("Enter the first number: "))
+        Num2= int(input("Enter the second number: "))
+    except:
+        print("Please enter numbers only!")
+        continue
 
-    Choice =input("Enter the operation you want to perform (+,-,*,/): ")
+    Choice =input("Enter operation (+, -, *, /): ")
 
-    def Calculator(Num1, Num2, Choice):
-        if Choice =="+":
-            return Num1 + Num2
-        elif Choice== "-":
-            return Num1- Num2
-        elif Choice =="*":
-            return Num1 * Num2
-        elif Choice == "/":
-            if Num2 ==0:
-                return "Error: Cannot divide by zero!"
-            return Num1/ Num2
-        else:
-            return "Invalid operation!"
+    if Choice not in ["+","-", "*", "/"]:
+        print("Invalid operation!")
+        continue
 
-    result = Calculator(Num1, Num2, Choice)
-    print("The Result is:",result)
+    result= Calculator(Num1, Num2, Choice)
+    print("The Result is:", result)
+    
+    Again =input("Do you want to calculate more? (Yes/No):").lower()
 
-    Again=input("Do you want to Calculate more? (Yes/No): ").lower()
-
-    if Again == "no":
+    if Again== "no":
         print("You Exit, Thank You!")
         break
+
+    if Again !="yes":
+        print("Please enter Yes or No!")
